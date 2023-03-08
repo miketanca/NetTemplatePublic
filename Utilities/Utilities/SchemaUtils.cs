752d246a27c1cc7c9b819c804676ecb5a20ec98f
michael tan <mtan@microclarity.com>
2023-03-08 17:58:59 -0500

    proper value compare

--- Utilities/Utilities/SchemaUtils.cs
+++ Utilities/Utilities/SchemaUtils.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
 namespace Utilities;
 
@@ -32,7 +33,18 @@ public static class SchemaUtils
     {
         var allContents = folders
             .SelectMany(x => Directory.GetFiles(x))
-            .OrderBy(x => Path.GetFileName(x))
+            .OrderBy(x =>
+            {
+                var name = Path.GetFileName(x);
+                var match = Regex.Match("V1_0_11", @"^V(\d+)_(\d+)_(\d+)");
+                var ns = new[]
+                {
+                    int.Parse(match.Groups[1].Value),
+                    int.Parse(match.Groups[2].Value),
+                    int.Parse(match.Groups[3].Value)
+                };
+                return 10000 * ns[0] + 100 * ns[1] + ns[2];
+            })
             .Select(x => File.ReadAllText(x))
             .Select(x => x + "\n");
 
